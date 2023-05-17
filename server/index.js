91961fd2e7ba46a4d7a9bec8a2d7ff0e9fb0b632
helmiaddibani <helmi.addibani@gmail.com>
2023-05-17 22:04:07 +0200

    Update index.js
    change the port 0000 for railway

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.get('/', async (req, res) => {
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => console.log('Server started on port 8080'));
+    app.listen(0000, () => console.log('Server started on port 8080'));
   } catch (error) {
     console.log(error);
   }
