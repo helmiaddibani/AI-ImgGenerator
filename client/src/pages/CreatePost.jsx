b3cc94269c694b64a0470c2877861ef398b0eabc
helmiaddibani <helmi.addibani@gmail.com>
2023-05-18 18:27:51 +0200

    Frontend ready to deploy

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -22,7 +22,7 @@ const generateImage = async () => {
   if (form.prompt) {
     try {
       setGeneratingImg(true);
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+      const response = await fetch('https://ai-imggenerator-backend-production.up.railway.app/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -51,7 +51,7 @@ const handleSubmit = async(e) => {
     setLoading(true)
 
     try {
-      const response = await fetch('http://localhost:8080/api/v1/post',{
+      const response = await fetch('https://ai-imggenerator-backend-production.up.railway.app/api/v1/post',{
         method: 'POST',
         headers:{
           'Content-Type': 'application/json',
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
       setLoading(true);
   
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://ai-imggenerator-backend-production.up.railway.app/api/v1/post', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
